57a058d028234a5503da06f9a49fc4f5f823fd06
Fernando Zuniga <fernando.zunigag@gmail.com>
2020-01-10 21:41:22 -0600

    adding tests Launchpad homepage

--- __tests__/homepage.ui.js
+++ __tests__/homepage.ui.js
@@ -1,52 +1,62 @@
 require("selenium-webdriver");
-const { expect } = chai;
-const { assert } = require('assert');
-const { chai } = require("chai");
 
-const { Home } = require("./pages/home");
-const { config } = require("../_config");
-const { utils } = require("../_utils");
+const chai = require("chai");
+chai.use(require("chai-as-promised"));
+const { expect } = chai;
+
+const Home = require("./pages/home");
+const config = require("../_config");
+const utils = require("../_utils");
+const assert = require('assert');
 
 const driverSetup = config.browser(process);
 
-chai.use(require("chai-as-promised"));
-
 describe("Home Page", () => {
   let driver;
-
   // beforeEach(async () => {
   before(async () => {
     driver = await driverSetup.build();
     home = new Home(driver);
     await home.open();
   });
-
   // afterEach(async () => {
   after(async () => {
     driver.quit();
   });
 
   describe("LaunchPad homepage items", () => {
-    it("should exist the Logo", async () => {
+    it("should exist the Launch Pad logo", async () => {
       const launchpPadLogo = await utils.waitFind(driver, home.locators.launchpagLogo);
       expect(launchpPadLogo).to.be.a("object");
-    });
-    it("should exist the Login button", async () => {
-      const loginIcon = await utils.waitFind(driver, home.locators.loginIcon);
-      expect(loginIcon).to.be.a("object");
     });
     it("should match the Header text", async () => {
       const launchPadHeader = await utils.waitFind(driver, home.locators.launchPadHeader);
       expect(launchPadHeader).to.be.a("object");
       assert.equal(await launchPadHeader.getText(), "Today's Shelter Information");
     });
+    it("should exist the Login button", async () => {
+      const launchPadLoginAvatar = await utils.waitFind(driver, home.locators.launchPadLoginAvatar);
+      expect(launchPadLoginAvatar).to.be.a("object");
+    });
   });
 
-  // describe("Perform login", () => {
-  //   it("successful login", async () => {
-  //     const header = await utils.waitFind(driver, home.locators.launchpagLogo);
-  //     expect(header).to.be.a("object");
-  //     await home.clickLoginIcon();
-  //   });
-  // });
+  describe("LaunchPad Site Director login page", () => {
+    it("should exist the Auth logo", async () => {
+      await home.clickLoginAvatar();
+      const authLogo = await utils.waitFind(driver, home.locators.authLogo);
+      expect(authLogo).to.be.a("object");
+    });
+    it("should match the Header text", async () => {
+      const authHeader = await utils.waitFind(driver, home.locators.authHeader);
+      expect(authHeader).to.be.a("object");
+      assert.equal(await authHeader.getAttribute("innerHTML"), "falcon");
+    });
+    it("should perform a successful login", async () => {
+      // TODO add some actions for login - issue that Auth0 don't allow actions on username and password
+      // const authUsername = await utils.waitFind(driver, home.locators.authUsername);
+      // expect(authUsername).to.be.a("object");
+      // await home.setUsername()
+    })
+  });
+
 });
--- __tests__/pages/home.js
+++ __tests__/pages/home.js
@@ -8,23 +8,29 @@ class HomePage {
     this.driver = driver;
     this.locators = {
       launchpagLogo: By.xpath('//*[@id="root"]/div/header/header/div/div/div[2]'),
-      loginIcon: By.xpath('//*[@id="root"]/div/header/header/div/div/div[4]/button'),
-      launchPadHeader: By.xpath('//*[@id="root"]/div/div/div/div/div/div[1]/h2')
+      launchPadLoginAvatar: By.xpath('//*[@id="root"]/div/header/header/div/div/div[4]/button'),
+      launchPadHeader: By.xpath('//*[@id="root"]/div/div/div/div/div/div[1]/h2'),
+
+      authLogo: By.xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div[1]/div[2]'),
+      authHeader: By.xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div[1]/div[2]/div'),
+      authUsername: By.xpath('//*[@id="1-email"]'),
+      authPassword: By.xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div/div/div[2]/div/div/input')
     }
   }
 
   open() {
     this.driver.get(url);
+    this.driver.manage().window().maximize();
   }
-  async clickLoginIcon() {
-    await this.driver.findElement(this.locators.loginIcon).click();
+  async clickLoginAvatar() {
+    await this.driver.findElement(this.locators.launchPadLoginAvatar).click();
   }
-  // async enterUsername() {
-  //   await this.driver.findElement(this.locators.username).sendKeys('xfernando@me.com');
-  // }
-  // async enterPassword() {
-  //   await this.driver.findElement(this.locators.password).sendKeys('Acklen11');
-  // }
+  async setUsername() {
+    await this.driver.findElement(this.locators.authUsername).sendKeys('xfernando@me.com');
+  }
+  async setPassword() {
+    await this.driver.findElement(this.locators.authPassword).sendKeys('Acklen11');
+  }
 }
 
 module.exports = HomePage;
